9cee7ff34643d9f0576c64e47cc293422f5c38f2
OdumahOdum <efuaodum20@gmail.com>
2022-10-31 08:40:32 -0700

    print sizes of functions

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -12,8 +12,8 @@ int main(void)
 	long long int longlongintType;
 	float floatType;
 
-	printf("Size of int: %zu bytes\n", sizeof(intType));
 	printf("Size of char: %zu bytes\n", sizeof(charType));
+	printf("Size of int: %zu bytes\n", sizeof(intType));
 	printf("Size of long int: %zu bytes\n", sizeof(longintType));
 	printf("Size of long long int: %zu bytes\n", sizeof(longlongintType));
 	printf("Size of float: %zu bytes\n", sizeof(floatType));
